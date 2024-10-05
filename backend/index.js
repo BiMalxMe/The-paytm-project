a60d7f789345d1f31f009ba0665aa982b90afc0f
BiMalxMe <journeyxbimal@gmail.com>
2024-10-05 10:12:21 +0545

    Vercel

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const mainRouter = require("./Routes/index");
 const cors = require("cors");
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin:['https://deploy-The-paytm-project.vercel.app'],
+    methods:["POST","GET"],
+    credentials:true
+}));
 app.use(express.json());
 app.use("/api/v1", mainRouter);
 app.listen(3000,()=>{console.log("Starting the server")});
